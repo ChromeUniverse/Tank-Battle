bc70f71b5141b21cd05cdd4abd297a26eed9b3e0
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2021-08-16 17:39:15 -0300

    Created login page API script

---
+++ public/js/auth/form-post.js
@@ -0,0 +1,58 @@
+
+async function postFormDataAsJson({ url, formData }) {
+	const plainFormData = Object.fromEntries(formData.entries());
+	const formDataJsonString = JSON.stringify(plainFormData);
+	console.log(formDataJsonString);
+
+	const fetchOptions = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			credentials: 'same-origin',			
+		},
+		body: formDataJsonString
+	};
+
+	const response = await fetch(url, fetchOptions);
+	const res_data = await response.json();
+
+	let redirect = false;
+
+	console.log(response.status);
+	
+	return [response.status, res_data];
+}
+
+
+async function handleFormSubmit(event) {
+	event.preventDefault();
+
+	const form = event.currentTarget;
+
+	console.log(form);
+
+	const url = form.action;
+
+	// const alert_msg = document.getElementById('alert-msg');
+	// alert_msg.innerText = '';
+
+	try {
+		const formData = new FormData(form);
+		// console.log('Here is the form data\n', formData);
+		const [statusCode, json] = await postFormDataAsJson({ url, formData });		
+
+		if (statusCode == 200) { window.location.assign('/'); }		
+		else {
+
+			const alert_msg = document.getElementById('alert-msg');
+			alert_msg.innerText = json.message;
+			
+		}
+
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+const login_form = document.getElementById("login-form");
+login_form.addEventListener("submit", handleFormSubmit);
