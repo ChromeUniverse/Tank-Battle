2c31cf7cbd338d4ec210f39dd6f613fbe451b938
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2021-08-16 17:39:07 -0300

    Created logout page API script

---
+++ public/js/auth/logout-api.js
@@ -0,0 +1,7 @@
+// let accessToken = localStorage.getItem('accessToken');
+
+// if (accessToken != null) {
+//     localStorage.removeItem("accessToken");    
+// } else {
+//     window.location.href = '/';
+// }
