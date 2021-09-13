a4f441c365d6faf1ebcc6849754b2a6a0328ece7
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2021-09-13 02:08:41 -0300

    Deleted unnecessary logout client-side script

--- public/js/auth/logout-api.js
+++
@@ -1,7 +0,0 @@
-// let accessToken = localStorage.getItem('accessToken');
-
-// if (accessToken != null) {
-//     localStorage.removeItem("accessToken");    
-// } else {
-//     window.location.href = '/';
-// }
