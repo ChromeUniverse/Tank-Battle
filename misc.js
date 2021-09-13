98900fe93d9bb8e8c72cd7c29e6de4d9cec2f929
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2021-09-13 02:22:13 -0300

    Added async HTML file I/O function to `misc.js`

--- misc.js
+++ misc.js
@@ -1,6 +1,7 @@
 // miscellaneous functions
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const { readFile } = require('fs/promises');
 
 function is_alpha_num(string) {
   return (string.match(/^[a-z0-9]+$/i) !== null);
@@ -22,4 +23,10 @@ function generateAccessToken(user) {
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
 }
 
-module.exports = { is_alpha_num, no_whitespace, hash, generateAccessToken }
+async function getHTML(name){
+  const path_to_htmls = '/home/lucca/Documents/Programming/tank_battle/private/html/'
+  const file = await readFile(path_to_htmls + name, 'UTF-8');
+  return file.toString();
+}
+
+module.exports = { is_alpha_num, no_whitespace, hash, generateAccessToken, getHTML }
