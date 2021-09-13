ade5995dde98eba208c2d99e397c7b8487263774
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2021-09-13 17:18:57 -0300

    Removed unnecessary code by calling custom module

--- routes/logout.js
+++ routes/logout.js
@@ -1,18 +1,14 @@
 // Package imports
 const express = require('express');
 const { private } = require('../middleware/private');
-const { readFile } = require('fs/promises');
+const { getHTML } = require('../misc');
 
 // Express Router setup
 const router = express.Router();
 router.use(private);
 
-// fs
-const path_to_htmls = '/home/lucca/Documents/Programming/tank_battle/private/html/';
-
 router.get('/', async (req, res)=> {  
-  const file = await readFile(path_to_htmls + 'logout.html', 'UTF-8');
-  res.status(200).clearCookie("token").send(file.toString());         
+  res.status(200).clearCookie("token").send(await getHTML('logout.html'));         
   return;
 });
 
--- routes/play.js
+++ routes/play.js
@@ -1,30 +1,25 @@
 // Package imports
 const express = require('express');
 const { private } = require('../middleware/private');
-const { readFile } = require('fs/promises');
-const { is_alpha_num, no_whitespace, hash, generateAccessToken } = require('../misc');
+const { is_alpha_num, getHTML } = require('../misc');
 const jwt = require('jsonwebtoken');
 
 // Express Router setup
 const router = express.Router();
 router.use(private);
 
-// fs
-const path_to_htmls = '/home/lucca/Documents/Programming/tank_battle/private/html/';
-
 router.get('/*', async (req, res) => {
 
   let endpoint = req.url.substr(1);
   console.log("Got Play request here:", endpoint);
 
-  if (!is_alpha_num(endpoint)) {return res.sendStatus(400)}
+  if (!is_alpha_num(endpoint)) { return res.status(400).send(await getHTML('400.html')) }
 
   const accessToken = jwt.decode(req.cookies.token);
 
   // replacing USERNAME, COLOR, ROOM with actual player name and random color
   res.status(200);
-  // res.redirect(301, 'http://yourotherdomain.com' + req.path)
-  let data = await readFile(path_to_htmls + 'room.html');
+  let data = await getHTML('room.html');
   res.send(
       data.toString()
           .replace('$USERNAME', accessToken.username)
