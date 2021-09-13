791665c3a167138922158ae599dcf2e78e5af919
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2021-09-13 02:34:57 -0300

    Improved comments for miscellaneous functions in `misc.js`

--- misc.js
+++ misc.js
@@ -1,16 +1,20 @@
 // miscellaneous functions
+
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const { readFile } = require('fs/promises');
 
+// checks if string contains only alphanumeric characters
 function is_alpha_num(string) {
   return (string.match(/^[a-z0-9]+$/i) !== null);
 }
 
+// checks if string doesn't contain any whitespace characters
 function no_whitespace(string) {
   return string.match(/^[^\s]+$/) !== null
 }
 
+// asynchronous bcrypt hashing function
 async function hash(input) {
   let salt = await bcrypt.genSalt(12);
   let hash = await bcrypt.hash(input, salt);
@@ -18,11 +22,12 @@ async function hash(input) {
   return hash;
 }
 
+// generate JWT access token
 function generateAccessToken(user) {
-  // return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '25s'});
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
 }
 
+// asynchronously fetch HTML file from "/private/" folder
 async function getHTML(name){
   const path_to_htmls = '/home/lucca/Documents/Programming/tank_battle/private/html/'
   const file = await readFile(path_to_htmls + name, 'UTF-8');
