3d5c2ce15a74235cb6ad6d584293beeae7720396
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2021-09-20 21:13:40 -0300

    Changed room state sent structure, added bullets

--- ws_modules/send_game_state.js
+++ ws_modules/send_game_state.js
@@ -8,16 +8,16 @@ function send_game_state_to_clients() {
   for (const [roomName, roomObject] of Object.entries(rooms)) {
 
     // preparing roomobject to send
-    let room_to_send = {};
+    // let room_to_send = {};
     
     // list of clients
     const clients = Object.values(roomObject.spectators);
 
-    let players = {};
-
-    // filtering out
+    // building players object to send
+    let players_to_send = {};
+    
     for (const [id, p] of Object.entries(roomObject.players)) {
-      players[id] = { 
+      players_to_send[id] = { 
         name: p.name,
         color: p.color,
         x: p.x, 
@@ -27,14 +27,27 @@ function send_game_state_to_clients() {
       }
     };
 
-    room_to_send.players = players;
+    // room_to_send.players = players;
+
+
+    // building players object to send
+    let bullets_to_send = [];
+    
+    for (const b of roomObject.bullets) {
+      let bullet_to_send = { x: b.x, y: b.y, name: b.name, color: b.color };
+      bullets_to_send.push(bullet_to_send);
+    };
+
+    // room_to_send.bullets = bullets;
+
 
     // sending room state to all clients in room
     for (const client of clients) {
       client.send(JSON.stringify(
         { 
           type: 'room-update',
-          room: room_to_send
+          players: players_to_send,
+          bullets: bullets_to_send
         } 
       ));
     }
