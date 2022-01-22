35afafb73216792d370f38b04bc36f680e2aba15
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-01-22 02:11:19 -0300

    Created basic views

---
+++ client/src/views/Leaderboard.vue
@@ -0,0 +1,28 @@
+<template>
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'Leaderboard',
+
+  data(){
+    return {
+
+    }
+  },
+
+  async created() {
+    const res = await fetch('http://localhost:4000/api/lb');
+    const data = res.json();
+    console.log(data);
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,45 @@
+<template>
+
+  <div id="login-form">
+
+    <div class="input-line">
+      <label for="username">Username</label>
+      <input type="text" id="username" name="username">
+    </div>
+
+    <div class="input-line">
+      <label for="password">Password</label>
+      <input type="password" id="password" name="password">
+    </div>
+    
+  </div>
+
+  
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      username: '',
+      password: '',
+    }
+  }
+}
+</script>
+
+<style>
+
+#login-form {
+  font-family: 'Roboto';
+}
+
+#login-form label {
+  color: var(--color5);
+}
+
+/* #login-form input {
+  color: var(--color1)
+} */
+
+</style>
