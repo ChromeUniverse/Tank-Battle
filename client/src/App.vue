3c3c1a90e7dd773bd7c73a487e35a8d58e7aad0c
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-01-22 02:11:49 -0300

    Created basic Vue app structure

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,57 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+<template> 
+  <Nav/>
   <router-view/>
+  <Footer/>
 </template>
 
+
+<script>
+
+import Nav from '@/components/Nav.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: {
+    Nav, Footer
+  },
+
+}
+</script>
+
+
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Recursive:wght@400;700&family=Roboto:ital,wght@0,400;0,700;1,700&display=swap');
+
+
+/* 
+
+  Color palletes:
+  https://coolors.co/32292f-575366-6e7dab-5762d5-d1e3dd
+  https://coolors.co/0a1128-001f54-034078-1282a2-fefcfb
+
+*/
+
+
+:root {
+  --color1: #0a1128;
+  --color2: #001f54;
+  --color3: #034078;
+  --color4: #1282a2;
+  --color5: #fefcfb;
+}
+
+body {
+  margin: 0px;
+  background-color: var(--color2);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', 'Recurssive', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  /* background-color: violet; */
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
