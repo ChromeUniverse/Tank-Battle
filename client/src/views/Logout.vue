e2328693a513f9cf0c674fbc919acd3f6fb3ba28
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-01-24 16:02:16 -0300

    Created Logout view

---
+++ client/src/views/Logout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="view" id="logout">
+    <h1>Logout</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logout',
+  async created(){
+
+    // if (!this.$store.state.auth){
+    //   console.log('Fetched store, already logged out! Redirecting...');
+    //   return this.$router.push('/');
+    // }
+
+    const res = await fetch('/api/logout');
+    console.log(res);
+    if (res.status == 200) {
+      console.log('LOGOUT!');
+      this.$store.commit('setAuth', false);
+      this.emitter.emit('logout');      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
