df9d4c28adb1de5df5d775eb678c0d4cd5673284
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-01-24 15:58:27 -0300

    Updated navbar styles, buttons, logic, events

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,30 +1,53 @@
 <template>
-  <div id="nav">
+  <div id="nav" class="header" v-cloak>
 
-    <NavButton text="Home" link=""/>
+    <div id="left">
+      <Button text="Home" link=""/>
+    </div>
 
-    <NavButton text="Leaderboard" link="lb"/>
+    <div id="center">
+      <Button text="Profile" link="user" v-if="auth"/>
+      <Button text="Rankings" link="lb" v-if="auth"/>
+      <Button text="About" link="about"/>
+      <Button text="Blog" link="http://34.200.98.64/" :external="true"/>
+      <Button text="Play Now!" link="play" :highlight="true" v-if="auth"/>
+    </div>
 
-    <NavButton text="Profile" link="user"/>
-
-    <NavButton text="Login" link="login"/>
-
-    <NavButton text="Signup" link="signup"/>
-
-    <NavButton text="Play Now!" link="play" :highlight="true"/>
+    <div id="right">      
+      <Button text="Login" link="play" :highlight="true" v-if="!auth"/>
+      <Button text="Logout" link="logout" v-if="auth"/>
+    </div>
 
   </div>
-
 </template>
 
 <script>
 
-import NavButton from '@/components/NavButton.vue'
+import Button from '@/components/Button.vue'
 
 export default {  
   name: 'Nav',
   components: {
-    NavButton
+    Button
+  }, 
+  // props: {
+    // auth: Boolean
+  // },
+
+  data(){
+    return {
+      auth: false
+    }
+  },
+
+  created(){
+    this.emitter.on('login', (evt) => {
+      this.auth = true;
+    })
+    this.emitter.on('logout', (evt) => {
+      this.auth = false;
+    })
+
   }
 }
 
@@ -33,21 +56,61 @@ export default {
 
 <style>
 
-#nav {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  padding: 10px;
-  border-radius: 3px;
+[v-cloak] {
+  display: none;
 }
 
-#nav a {
-  font-family: 'Recursive';
-  padding: 5px 10px;
-  margin: 0px 5px;
-  font-size: 1em;
-  text-decoration: none;
-  border-radius: 6px;
+#nav {
+  display: grid;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 15px 100px 0px 100px;
+  /* margin: 30px 0px 0px 0px; */
+  /* margin: auto; */
+  /* border-radius: 0px 0px 12px 12px; */
+  background-color: var(--color7);
+  grid-template-columns: [first] 20% auto [last] 20%;
 }
+
+#left {
+  text-align: left;
+} 
+
+#center {
+  /* justify-self: center; */
+  /* use either */
+  margin: 0 auto;
+  /* display: flex; */
+
+}
+
+#right {
+  /* justify-self: end; */
+  text-align: right;
+}
+
+/* #left, #right {
+  flex-grow: 1;
+  flex-basis: 0;
+} */
+
+.header {
+  /* fixing the position takes it out of html flow - knows
+  nothing about where to locate itself except by browser
+  coordinates */
+  position:fixed; 
+  /* top left corner should start at leftmost spot */
+  /* left:0;            */
+  /* top left corner should start at topmost spot */
+  top: 0px;            
+  width:95vw;      
+  left: 50%;
+  transform: translate(-50%, 0);
+  /* high z index so other content scrolls underneath */
+  z-index:200;  
+  /* define height for content */
+  height: 35px;     
+}
+
 
 </style>
